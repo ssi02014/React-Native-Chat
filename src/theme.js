70875428d5eaa6b57c637d83c0b06918b958f8a9
JeonMinJae <ssi02014@naver.com>
2021-04-09 15:42:47 +0900

    Add(minjae): 로고 적용하기 #5

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,14 @@
 const colors = {
-    white: '#ffffff',
-    black: '#000000',
-    grey_0: '#d5d5d5',
-    grey_1: '#a6a6a6',
-    red: '#e84118',
-    blue: '#3679fe',
+  white: "#ffffff",
+  black: "#000000",
+  grey_0: "#d5d5d5",
+  grey_1: "#a6a6a6",
+  red: "#e84118",
+  blue: "#3679fe",
 };
 
 export const theme = {
-    background: colors.white,
-    text: colors.black,
-}
+  background: colors.white,
+  text: colors.black,
+  imageBackground: colors.grey_0,
+};
---
+++ src/utils/images.js
@@ -0,0 +1,6 @@
+const prefix =
+  "https://firebasestorage.googleapis.com/v0/b/react-native-chat-65246.appspot.com/o";
+
+export const images = {
+  logo: `${prefix}/logo.png?alt=media`,
+};
