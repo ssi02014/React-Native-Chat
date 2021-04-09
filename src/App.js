ccf18ecc59b0f5beeddff7d9ee9903cc9ad8ee7a
JeonMinJae <ssi02014@naver.com>
2021-04-09 11:39:16 +0900

    Add(minjae/appLoading): appLoading, icon Image Update #3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from "react";
-import { StatusBar, Text } from "react-native";
+import React, { useState } from "react";
+import { StatusBar, Text, Image } from "react-native";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./theme";
 
+const cacheImages = (images) => {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
+
+const cacheFonts = (fonts) => {
+  return fonts.map((font) => Font.loadAsync(font));
+};
+
 const App = () => {
-  return (
+  const [isReady, setIsReady] = useState(false);
+
+  const _loadAssets = async () => {
+    const imageAssets = cacheImages([require("../assets/splash.png")]);
+    const fontAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  };
+
+  return isReady ? (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" />
-      <Text>Chat App Setting</Text>
-      <Text>Chat App Setting</Text>
-      <Text>Chat App Setting</Text>
+      <Text>hi</Text>
     </ThemeProvider>
+  ) : (
+    <AppLoading
+      startAsync={_loadAssets}
+      onFinish={() => setIsReady(true)}
+      onError={console.error}
+    />
   );
 };
 
