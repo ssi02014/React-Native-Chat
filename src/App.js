978b85adb3d1674612e798a5e4e816f787ddc437
JeonMinJae <ssi02014@naver.com>
2021-04-09 11:07:42 +0900

    Add(minjae/projectSetting): firebase Setting #1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { StatusBar, Text } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import { theme } from './theme';
+import React from "react";
+import { StatusBar, Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./theme";
 
 const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <StatusBar barStyle="light-content" />
-            <Text>Chat App Setting</Text>
-            <Text>Chat App Setting</Text>
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" />
+      <Text>Chat App Setting</Text>
+      <Text>Chat App Setting</Text>
+      <Text>Chat App Setting</Text>
+    </ThemeProvider>
+  );
+};
 
 export default App;
