bd465dfca269484d0481422dab6a7052e4f90cc9
JeonMinJae <ssi02014@naver.com>
2021-04-12 13:14:59 +0900

    Add(minjae/auth): 사진 입력 Icon #5

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Container = styled.View`
   align-self: center;
   margin-bottom: 30px;
+`;
+
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: ${({ theme }) => theme.ImageButtonBackground};
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonIcon = styled(MaterialIcons).attrs({
+  name: "photo-camera",
+  size: 22,
+})`
+  color: ${({ theme }) => theme.ImageButtonIcon};
 `;
 
 const StyledImage = styled.Image`
@@ -14,22 +34,33 @@ const StyledImage = styled.Image`
   border-radius: ${({ rounded }) => (rounded ? 50 : 0)}px;
 `;
 
-const Image = ({ url, imageStyle, rounded }) => {
+const PhotoButton = ({ onPress }) => {
+  return (
+    <ButtonContainer onPress={onPress}>
+      <ButtonIcon />
+    </ButtonContainer>
+  );
+};
+
+const Image = ({ url, imageStyle, rounded, showButton }) => {
   return (
     <Container>
       <StyledImage source={{ uri: url }} style={imageStyle} rounded={rounded} />
+      {showButton && <PhotoButton />}
     </Container>
   );
 };
 
 Image.defaultProps = {
   rounded: false,
+  showButton: false,
 };
 
 Image.propTypes = {
   url: PropTypes.string,
   imageStyle: PropTypes.object,
   rounded: PropTypes.bool,
+  showButton: PropTypes.bool,
 };
 
 export default Image;
