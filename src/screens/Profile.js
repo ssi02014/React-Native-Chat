c8dcb373ef985e889f8fa78ad30494adb89d08ff
JeonMinJae <ssi02014@naver.com>
2021-04-14 13:37:48 +0900

    Add(minjae/profile): profile UI #12

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,32 +1,65 @@
-import React, { useContext } from "react";
-import styled from "styled-components/native";
-import { Text } from 'react-native';
-import { Button } from '../components';
-import { logout } from '../utils/firebase';
-import { UserContext } from '../contexts';
+import React, { useContext, useState } from "react";
+import styled, { ThemeContext } from "styled-components/native";
+import { Button, Image, Input } from '../components';
+import { getCurrentUser, logout, updateUserPhoto } from '../utils/firebase';
+import { ProgressContext, UserContext } from '../contexts';
+import { Alert } from 'react-native';
 
 const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.background};
+    justify-content: center;
+    align-items: center;
+    padding: 0 20px;
 `;
 
 const Profile = () => {
     const { dispatch } = useContext(UserContext);
+    const { spinner } = useContext(ProgressContext);
+    const theme = useContext(ThemeContext);
+
+    const user = getCurrentUser();
+    const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
 
     const _handleLogoutButtonPress = async () => {
         try {
+            spinner.start();
             await logout();
         } catch (e) {
             console.log('[Profile] logout: ', e.message);
         } finally {
             dispatch({});
+            spinner.stop();
         };
+    };
+
+    const _handlePhotoChange = async url => {
+        try {
+            spinner.start();
+            const updateUser = await updateUserPhoto(url);
+            setPhotoUrl(updateUser.photoUrl);
+        } catch (e) {
+            Alert.alert('Photo Error', e.message);
+        } finally {
+            spinner.stop();
+        }
     };
 
     return (
         <Container>
-            <Text style={{ fontSize: 24 }}>Profile</Text>
-            <Button title="Logout" onPress={_handleLogoutButtonPress} />
+            <Image 
+                url={photoUrl}
+                onChangeImage={_handlePhotoChange}
+                showButton
+                rounded
+            />
+            <Input label="Name" value={user.name} />
+            <Input label="Email" value={user.email} />
+            <Button 
+                title="Logout" 
+                onPress={_handleLogoutButtonPress}
+                containerStyle={{ marginTop: 30, backgroundColor: theme.buttonLogout }}
+            />
         </Container>
     )
 };
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -47,3 +47,17 @@ export const signup = async ({ email, password, name, photoUrl }) => {
 export const logout = async () => {
   return await Auth.signOut();
 };
+
+export const getCurrentUser = () => {
+  const { uid, displayName, email, photoURL } = Auth.currentUser;
+  return { uid, name: displayName, email, photoUrl: photoURL };
+};
+
+export const updateUserPhoto = async photoUrl => {
+  const user = Auth.currentUser;
+  const storageUrl = photoUrl.startsWith('https')
+    ? photoUrl
+    : await uploadImage(photoUrl);
+  await user.updateProfile({ photoURL: storageUrl });
+  return { name: user.displayName, email: user.email, photoUrl: user.photoURL };
+};
